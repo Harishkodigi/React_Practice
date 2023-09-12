66eebccaa3af80d51896c856bd1539022fa91a9e
Harishkumar <harishkumar.kodigi@gmail.com>
2023-09-12 13:41:10 +0530

    introducing React Hook concept using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import resData from "../utils/mockData";
+import { useState } from "react";
 
 
 const Body = () => {
+
+    const [restaurantList, setRestaurantList] = useState(resData);
+
     return (
         <div className="body" >
-            <div className="search">search</div>
+            <div className="filter">
+                <button className="filter-btn"
+                    onClick={() => {
+                        const filterdData = resData.filter((res) => res.info.avgRating > 4.5);
+
+                        setRestaurantList(filterdData)
+                    }}>
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-container" >
-                {resData.map((restaurant) =>
+                {restaurantList.map((restaurant) =>
                     (<RestaurantCard key={restaurant.info.id} resdata={restaurant} />))}
 
             </div>
